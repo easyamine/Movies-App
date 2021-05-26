c35abab8029024a9dca04c7ff125b4209f56de9c
easyamine <aminmerz1700@gmail.com>
2021-05-26 12:49:25 +0100

    movies

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Component/App.js'
+import Styles from "./Styles/style.css";
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
